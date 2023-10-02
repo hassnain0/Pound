42581411329d36424520ff181e0c6c4a6d971851
hassnain0 <hassnainalidayo6@gmail.com>
2023-10-02 23:58:53 +0500

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './Screens/Home';
+import Chat from './Screens/Chat';
+
+const Tab = createBottomTabNavigator();
+
+function Camera(){
+  
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Tab.Navigator>
+      <Tab.Screen name='Home' component={Home} tabBarOptions={{
+          activeTintColor: 'green', // Color of the active tab label
+          inactiveTintColor: 'black', // Color of inactive tab labels
+          style: { backgroundColor: 'black' },
+          outerHeight:20
+           // Background color of the tab bar
+        }}
+        options={{headerShown:false,}}/>
+      <Tab.Screen name='Camera' component={Camera} options={{headerShown:false}}/>   
+      <Tab.Screen name='Chat' component={Chat} options={{headerShown:false}}/>     
+     </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Screens/Chat.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Chat=()=>{
+    <View></View>
+}
+export default Chat;
