42882fff5efb990c123ca7feedb996fbdc6628bd
hassnain0 <hassnainalidayo6@gmail.com>
2023-10-03 12:36:12 +0500

    After Tab Navigation

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Screens/Home';
 import Chat from './Screens/Chat';
+import { Image } from 'react-native';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -13,17 +15,52 @@ function Camera(){
 export default function App() {
   return (
     <NavigationContainer>
-     <Tab.Navigator>
+     <Tab.Navigator >
       <Tab.Screen name='Home' component={Home} tabBarOptions={{
-          activeTintColor: 'green', // Color of the active tab label
+          activeTintColor: '#8DCF02', // Color of the active tab label
           inactiveTintColor: 'black', // Color of inactive tab labels
-          style: { backgroundColor: 'black' },
-          outerHeight:20
+  
+          tabBarStyle: {
+            backgroundColor: 'Black', // Set the background color of the tab bar
+          },
+          style: {
+            backgroundColor: 'black', // Set the background color of the tab bar
+          },    
+          
            // Background color of the tab bar
         }}
-        options={{headerShown:false,}}/>
-      <Tab.Screen name='Camera' component={Camera} options={{headerShown:false}}/>   
-      <Tab.Screen name='Chat' component={Chat} options={{headerShown:false}}/>     
+        options={{ tabBarIcon: ({ focused, color, size }) => (
+          <Image
+            source={require('./assets/Home.png')} // Replace 'your-icon.png' with the actual icon file name in the root folder
+            style={{ width: 10, height: 10, tintColor: color, }}
+          />
+        ),}}/>
+      <Tab.Screen name='Camera' component={Camera}   options={{ tabBarIcon: ({ focused, color, size }) => (
+          <Image
+            source={require('./assets/myCamera.png')} // Replace 'your-icon.png' with the actual icon file name in the root folder
+            style={{ width: size, height: size, tintColor: color, }}
+          />
+        ),}} tabBarOptions={{
+          activeTintColor: '#8DCF02', // Color of the active tab label
+          inactiveTintColor: 'black', // Color of inactive tab labels
+          style: { backgroundColor: 'black' },
+          outerHeight:30
+           // Background color of the tab bar
+        }}/>   
+      <Tab.Screen name='Chat' component={Chat}   options={{ tabBarIcon: ({ focused, color, size }) => (
+          <Image
+            source={require('./assets/Vector.png')} // Replace 'your-icon.png' with the actual icon file name in the root folder
+            style={{ width: size, height: size, tintColor: color, }}
+          />
+        ),}} tabBarOptions={{
+          activeTintColor: '#8DCF02', // Color of the active tab label
+          inactiveTintColor: 'white', // Color of inactive tab labels
+          outerHeight:30, style: { backgroundColor: 'black' },
+          
+           // Background color of the tab bar
+        }
+      }
+        />     
      </Tab.Navigator>
     </NavigationContainer>
   );
--- Screens/Chat.js
+++ Screens/Chat.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const Chat=()=>{
-    <View></View>
-}
+const Chat = () => {
+  return (
+    <View style={styles.ViewContainer}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ViewContainer: {
+    flex: 1,
+    backgroundColor: 'black', // Set the background color to black
+  },
+});
+
 export default Chat;
